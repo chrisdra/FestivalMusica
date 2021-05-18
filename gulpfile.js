f16654c03da983cfe70366fac145b9efb018f50b
Christian Herrera Cabezas <chrisdra.hc@gmail.com>
2021-05-18 03:42:09 -0400

    Modificacion npg a webp
    Se trasnforma varios archivos de PNG a WebP, por otro lado tambien se empieza a trabajar en la parte de Line UP.

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,52 @@
-const { series, src, dest, watch} = require('gulp');
+const { series, src, dest, watch, parallel} = require('gulp');
 const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+const notify = require('gulp-notify');
+const webp = require('gulp-webp');
 
 // Funcion que compila SASS
 
+const paths = {
+    imagenes: 'src/img/**/*',
+    scss: 'src/scss/**/*.scss',
+}
+
 function css () {
-    return src('src/scss/app.scss')
+    return src(paths.scss)
     .pipe( sass())
     .pipe( dest('./build/css'))
 }
 
 function minificarcss () {
-    return src('src/scss/app.scss')
+    return src(paths.scss)
     .pipe( sass({
         outputStyle: 'compressed'
     }))
     .pipe( dest('./build/css'))
 }
 
+function imagenes() {
+    return src(paths.imagenes)
+        .pipe( imagemin() )
+        .pipe( dest( './build/img' ))
+        .pipe( notify({message: 'Imagen Minificada'}));
+}
+
+function versionWebp() {
+    return src(paths.imagenes)
+        .pipe( webp() )
+        .pipe(dest('./build/img'))
+        .pipe( notify({message: 'Versiíon webP lista'}));
+}
+
 function watchArchivos() {
-    watch( 'src/scss/**/*.scss', css ); // * = la carpeta actual - ** = Todos los archivos con esa extension
+    watch( paths.scss, css ); // * = la carpeta actual - ** = Todos los archivos con esa extension
 }
 
 exports.css = css;
 exports.minificarcss = minificarcss;
+exports.imagenes = imagenes;
 exports.watchArchivos = watchArchivos;
+
+//esto ejecuta todo al mismo tiempo en vez de estar escribiendo uno por uno
+exports.default = series( css, imagenes, versionWebp, watchArchivos );
